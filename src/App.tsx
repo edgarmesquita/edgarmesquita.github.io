98edc9eb944dcf54e50b3e8904840dcfaecbcaf0
Edgar Mesquita <edgar.rj@gmail.com>
2018-10-04 21:49:22 -0300

    ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import * as React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+// import createHistory from 'history/createBrowserHistory';
 import { Provider } from './store';
 import HomeView from './views/HomeView';
 import VendasView from './views/VendasView';
-import './scss/style.css'
+import './scss/style.css';
+
+// const history = createHistory({
+//     basename: process.env.PUBLIC_URL,
+// });
+
 export default class App extends React.Component {
     public render() {
         return (
             <Provider>
-                <Switch>
-                    <Route exact={true} path='/' component={HomeView} />
-                    <Route exact={true} path='/vendas' component={VendasView} />
-                </Switch>
+                <BrowserRouter basename={process.env.PUBLIC_URL}>
+                    <Switch>
+                        <Route exact={true} path='/' component={HomeView} />
+                        <Route exact={true} path='/vendas' component={VendasView} />
+                    </Switch>
+                </BrowserRouter>
             </Provider>
         );
     }
