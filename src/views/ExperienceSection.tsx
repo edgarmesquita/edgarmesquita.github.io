0b878b1f848a039849fdb8c1c2afbffe9ba4738e
Edgar Mesquita <edgar.rj@gmail.com>
2023-02-08 21:56:01 +0000

    update experiences

--- src/views/ExperienceSection.tsx
+++ src/views/ExperienceSection.tsx
@@ -6,123 +6,137 @@ import TimelineList from './TimelineList';
 
 const boxBackgroundColor = 'rgba(0,0,0,.1)';
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
+  createStyles({
 
-    }),
+  }),
 );
 
 
 export interface IExperienceSectionProps {
-    className?: string;
-    titleClassName?: string;
+  className?: string;
+  titleClassName?: string;
 }
 
 const experience: ITimelineItem[] = [
-    {
-        title: "🇵🇹 Anova", role: "Team Leader",
-        description: "Team follow-up, technical planning, product scalability. A strong will to continuously learn, teach, improve and make others grow.",
-        startDate: "Apr 2022",
-        endDate: "Present", position: 'left'
-    },
-    {
-        title: "🇵🇹 Anova", role: "Senior Backend Developer",
-        description: "Development and maintenance of a cloud-native IoT platform for the global leader in remote monitoring of industrial assets: Industrial Gas, Rail, Energy, Oil and Gas, Water / Sewer and other industries.",
-        startDate: "Apr 2020",
-        endDate: "Present", position: 'left'
-    },
-    {
-        title: "🇵🇹 Farfetch", role: "Senior Software Engineer",
-        description: "Maintenance and creation of new financial microservices, with development of RESTful APIs and frameworks.",
-        startDate: "Dec 2018",
-        endDate: "Apr 2020", position: 'left'
-    },
-    {
-        title: "🇧🇷 Plah Tecnologia", role: "Founder & CTO",
-        description: "Responsible for the management, architecture and development of social networking, highly scalable and integrated with Azure/Amazon services. Total participation in all stages, including UI / UX (both web, the mobile). Web publishing, Apple and Google Play stores.",
-        startDate: "Mar 2015",
-        endDate: "Dec 2018", position: 'left'
-    },
-    {
-        title: "🇧🇷 Módulo Security", role: "Senior Software Analysis",
-        description: "Architecture and development of systems focused on solutions for GRC (Governance, Risk and Compliance); BI modules development integrated with Microsoft SharePoint and Analysis Service for national large-scale projects (World Cup 2014).",
-        startDate: "Dec 2013",
-        endDate: "Nov 2014", position: 'left'
-    },
-    {
-        title: "🇧🇷 Qx3", role: "Specialist .Net Developer",
-        description: "Monitoring and participation in project architecture processes for Banco Itaú. Development of the IPO platform of the BTG Pactual bank.",
-        startDate: "Jan 2012",
-        endDate: "Nov 2012", position: 'left'
-    },
-    {
-        title: "🇧🇷 Qx3", role: "Senior .Net Developer",
-        description: "Maintenance and development of financial systems in ASP.Net (C # MVC2 / 3) + SQL Server / Oracle, Python + Django + MySQL; Definition of the functional structure of the projects and communication with Stakeholders; SCRUM Methodology",
-        startDate: "Jul 2011",
-        endDate: "Jan 2012", position: 'left'
-    },
-    {
-        title: "🇧🇷 BRQ", role: "Mid System Analyst and Developer",
-        description: "Third-party at \"Icatu Seguros\" Company: Maintenance and development of systems in ASP.Net (VB and C #) + SQL Server integrated to Lumis Portal Suite 3; Maintenance of products such as \"Cliente Online\", \"Casa do Corretor\", \"Site de Serviços\", etc .; Functional and technical documentation, IBM Tools (Rational Clear Case and Ration Clear Quest).",
-        startDate: "Nov 2010",
-        endDate: "Apr 2011", position: 'right'
-    },
-    {
-        title: "🇧🇷 TO Brasil", role: "System Analyst Developer",
-        description: "Third-party at \"Lojas Americanas\" Company: Maintenance of DP / HR management systems; Development of treasury systems, reconciliation of credit card: control of daily transactions, reports, etc.",
-        startDate: "Nov 2009",
-        endDate: "Oct 2010", position: 'right'
-    },
-    {
-        title: "🇧🇷 Running Solutions", role: "System Analyst Developer",
-        description: "Maintenance and development of corporate portals, intranet and web systems using languages such as: ASP.Net, PHP 5, ASP and database: SQL Server, Oracle, MySQL, DB2, Access",
-        startDate: "Jun 2008",
-        endDate: "Jun 2009", position: 'right'
-    },
-    {
-        title: "🇧🇷 Running Solutions", role: "Mid Web Designer",
-        startDate: "Nov 2007",
-        endDate: "Jun 2008", position: 'right'
-    },
-    {
-        title: "🇧🇷 Addtech", role: "Mid Web Designer",
-        description: "Creation and development of corporate portals using Lumis Portal Suite (Content Management System - CMS); Use of tools such as: Flash, Fireworks, Dreamweaver, CorelDraw, Photoshop, 3D Studio, Maya; and languages like: Action Script, Java Script, HTML, CSS, XML / XSL, PHP. I also participated in the development of multimedia material, flash systems for desktop and internal stationery (Folders, cards, labels).",
-        startDate: "Oct 2004",
-        endDate: "Jun 2006", position: 'right',
-        projects: [
-            { 
-                title: "Portal Cultura Inglesa", 
-                description: "Development of navigation structure; Implementation in the Lumis Portal Manager tool."
-            },
-            { 
-                title: "Portal Golden Cross", 
-                description: "Elaboration of banners for portal campaigns. Content implementation."
-            },
-            { 
-                title: "Portal Mongeral", 
-                description: "Implementation in the Lumis Portal Manager tool; Development of Flash presentations."
-            },
-            { 
-                title: "Intranet Unimed Rio", 
-                description: "Development of the interface, implementation in the Lumis Portal Manager tool."
-            }
-        ]
-    }
+  {
+    title: "🇵🇹 eQuantic Tech", role: "Founder",
+    description: "Consultancy focused on solving complex problems supported by highly qualified professionals.",
+    startDate: "Feb 2008",
+    endDate: "Present", position: 'left',
+    projects: [
+      {
+        title: "We build powerful business services",
+        description: "eQuantic Systems promotes and supports startups to develop innovation and disruptive technology. We seek to solve the needs of the market.",
+        url: "https://equantic.tech",
+        image: "/img/equantic-icon.png"
+      }
+    ]
+  },
+  {
+    title: "🇵🇹 Anova", role: "Team Leader",
+    description: "Team follow-up, technical planning, product scalability. A strong will to continuously learn, teach, improve and make others grow.",
+    startDate: "Apr 2022",
+    endDate: "Dec 2022", position: 'left'
+  },
+  {
+    title: "🇵🇹 Anova", role: "Senior Backend Developer",
+    description: "Development and maintenance of a cloud-native IoT platform for the global leader in remote monitoring of industrial assets: Industrial Gas, Rail, Energy, Oil and Gas, Water / Sewer and other industries.",
+    startDate: "Apr 2020",
+    endDate: "Dec 2022", position: 'left'
+  },
+  {
+    title: "🇵🇹 Farfetch", role: "Senior Software Engineer",
+    description: "Maintenance and creation of new financial microservices, with development of RESTful APIs and frameworks.",
+    startDate: "Dec 2018",
+    endDate: "Apr 2020", position: 'left'
+  },
+  {
+    title: "🇧🇷 Plah Tecnologia", role: "Founder & CTO",
+    description: "Responsible for the management, architecture and development of social networking, highly scalable and integrated with Azure/Amazon services. Total participation in all stages, including UI / UX (both web, the mobile). Web publishing, Apple and Google Play stores.",
+    startDate: "Mar 2015",
+    endDate: "Dec 2018", position: 'left'
+  },
+  {
+    title: "🇧🇷 Módulo Security", role: "Senior Software Analysis",
+    description: "Architecture and development of systems focused on solutions for GRC (Governance, Risk and Compliance); BI modules development integrated with Microsoft SharePoint and Analysis Service for national large-scale projects (World Cup 2014).",
+    startDate: "Dec 2013",
+    endDate: "Nov 2014", position: 'left'
+  },
+  {
+    title: "🇧🇷 Qx3", role: "Specialist .Net Developer",
+    description: "Monitoring and participation in project architecture processes for Banco Itaú. Development of the IPO platform of the BTG Pactual bank.",
+    startDate: "Jan 2012",
+    endDate: "Nov 2012", position: 'left'
+  },
+  {
+    title: "🇧🇷 Qx3", role: "Senior .Net Developer",
+    description: "Maintenance and development of financial systems in ASP.Net (C # MVC2 / 3) + SQL Server / Oracle, Python + Django + MySQL; Definition of the functional structure of the projects and communication with Stakeholders; SCRUM Methodology",
+    startDate: "Jul 2011",
+    endDate: "Jan 2012", position: 'left'
+  },
+  {
+    title: "🇧🇷 BRQ", role: "Mid System Analyst and Developer",
+    description: "Third-party at \"Icatu Seguros\" Company: Maintenance and development of systems in ASP.Net (VB and C #) + SQL Server integrated to Lumis Portal Suite 3; Maintenance of products such as \"Cliente Online\", \"Casa do Corretor\", \"Site de Serviços\", etc .; Functional and technical documentation, IBM Tools (Rational Clear Case and Ration Clear Quest).",
+    startDate: "Nov 2010",
+    endDate: "Apr 2011", position: 'right'
+  },
+  {
+    title: "🇧🇷 TO Brasil", role: "System Analyst Developer",
+    description: "Third-party at \"Lojas Americanas\" Company: Maintenance of DP / HR management systems; Development of treasury systems, reconciliation of credit card: control of daily transactions, reports, etc.",
+    startDate: "Nov 2009",
+    endDate: "Oct 2010", position: 'right'
+  },
+  {
+    title: "🇧🇷 Running Solutions", role: "System Analyst Developer",
+    description: "Maintenance and development of corporate portals, intranet and web systems using languages such as: ASP.Net, PHP 5, ASP and database: SQL Server, Oracle, MySQL, DB2, Access",
+    startDate: "Jun 2008",
+    endDate: "Jun 2009", position: 'right'
+  },
+  {
+    title: "🇧🇷 Running Solutions", role: "Mid Web Designer",
+    startDate: "Nov 2007",
+    endDate: "Jun 2008", position: 'right'
+  },
+  {
+    title: "🇧🇷 Addtech", role: "Mid Web Designer",
+    description: "Creation and development of corporate portals using Lumis Portal Suite (Content Management System - CMS); Use of tools such as: Flash, Fireworks, Dreamweaver, CorelDraw, Photoshop, 3D Studio, Maya; and languages like: Action Script, Java Script, HTML, CSS, XML / XSL, PHP. I also participated in the development of multimedia material, flash systems for desktop and internal stationery (Folders, cards, labels).",
+    startDate: "Oct 2004",
+    endDate: "Jun 2006", position: 'right',
+    projects: [
+      {
+        title: "Portal Cultura Inglesa",
+        description: "Development of navigation structure; Implementation in the Lumis Portal Manager tool."
+      },
+      {
+        title: "Portal Golden Cross",
+        description: "Elaboration of banners for portal campaigns. Content implementation."
+      },
+      {
+        title: "Portal Mongeral",
+        description: "Implementation in the Lumis Portal Manager tool; Development of Flash presentations."
+      },
+      {
+        title: "Intranet Unimed Rio",
+        description: "Development of the interface, implementation in the Lumis Portal Manager tool."
+      }
+    ]
+  }
 ];
 
 const ExperienceSection = ({ className, titleClassName }: IExperienceSectionProps) => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
+  return (
 
-        <Container maxWidth="md" id="experience" className={className}>
-            <Typography variant="h1" component="h2" className={titleClassName}>
-                Experience <Typography component="span" variant="caption">Most relevants</Typography>
-            </Typography>
+    <Container maxWidth="md" id="experience" className={className}>
+      <Typography variant="h1" component="h2" className={titleClassName}>
+        Experience <Typography component="span" variant="caption">Most relevants</Typography>
+      </Typography>
 
-            <TimelineList items={experience} />
-        </Container>
+      <TimelineList items={experience} />
+    </Container>
 
-    );
+  );
 }
 
 export default ExperienceSection;
--- src/views/TimelineItem.tsx
+++ src/views/TimelineItem.tsx
@@ -1,109 +1,136 @@
 import * as React from 'react';
 import { createStyles, makeStyles, useTheme, Theme } from '@material-ui/core/styles';
-import { Box, Card, CardContent, Typography } from '@material-ui/core';
+import { Box, Card, CardActionArea, CardContent, Typography } from '@material-ui/core';
 
 import {
-    IoBriefcaseOutline
+  IoBriefcaseOutline
 } from "react-icons/io5";
 import { IconType } from 'react-icons';
 
 const boxBackgroundColor = 'rgba(0,0,0,.1)';
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
+  createStyles({
 
-        boxIcon: {
-            position: 'absolute',
-            left: theme.spacing(2.4),
-            fontSize: '1.8rem',
-            color: theme.palette.secondary.dark
-        },
-        boxTitle: {
-            fontWeight: 700,
-        },
-        boxSubtitle: {
-            marginTop: theme.spacing(-.5),
-        },
-        boxCaption: {
-            color: 'rgba(255,255,255,.5)',
-            paddingBottom: theme.spacing(1.5)
-        },
-        boxText: {
+    boxIcon: {
+      position: 'absolute',
+      left: theme.spacing(2.4),
+      fontSize: '1.8rem',
+      color: theme.palette.secondary.dark
+    },
+    boxTitle: {
+      fontWeight: 700,
+    },
+    boxSubtitle: {
+      marginTop: theme.spacing(-.5),
+    },
+    boxCaption: {
+      color: 'rgba(255,255,255,.5)',
+      paddingBottom: theme.spacing(1.5)
+    },
+    boxText: {
 
-        },
-        timeline: {
-            borderLeft: '2px solid',
-            borderLeftColor: theme.palette.secondary.dark,
-            padding: theme.spacing(1, 0, 1, 3)
-        },
-        project: {
-            marginTop: theme.spacing(2),
-            backgroundColor: theme.palette.primary.main
-        },
-        projectContent: {
-            padding: theme.spacing(1, 2),
-            paddingBottom: theme.spacing(1) + 'px !important'
-        },
-        projectTitle: {
-            fontSize: '1rem'
-        },
-        projectDescription: {
-            fontSize: '.8rem'
-        }
-    }),
+    },
+    timeline: {
+      borderLeft: '2px solid',
+      borderLeftColor: theme.palette.secondary.dark,
+      padding: theme.spacing(1, 0, 1, 3)
+    },
+    project: {
+      marginTop: theme.spacing(2),
+      backgroundColor: theme.palette.primary.main,
+    },
+    projectAction: {
+      display: 'flex',
+      alignItems:  'flex-start'
+    },
+    projectContent: {
+      padding: theme.spacing(1, 2, 1, 1),
+      paddingBottom: theme.spacing(1) + 'px !important'
+    },
+    projectTitle: {
+      fontSize: '1rem'
+    },
+    projectDescription: {
+      fontSize: '.8rem'
+    },
+    projectImageContent: {
+      maxWidth: 60,
+      padding: theme.spacing(1, 0),
+    },
+    projectImage: {
+      width: '100%',
+    }
+  }),
 );
 
 export interface IProject {
-    title: string;
-    description: string;
-    date?: string;
-    url?: string;
+  title: string;
+  description: string;
+  date?: string;
+  url?: string;
+  image?: string;
 }
 export interface ITimelineItem {
-    title: string;
-    role: string;
-    description?: string | JSX.Element;
-    startDate: string;
-    endDate?: string;
-    projects?: IProject[];
-    position: 'left' | 'right'
+  title: string;
+  role: string;
+  description?: string | JSX.Element;
+  startDate: string;
+  endDate?: string;
+  projects?: IProject[];
+  position: 'left' | 'right'
 }
 
 export interface ITimelineItemProps {
-    item: ITimelineItem,
-    iconType?: IconType,
-    continuation?: boolean
+  item: ITimelineItem,
+  iconType?: IconType,
+  continuation?: boolean
 }
 const TimelineItem = ({ item, iconType, continuation }: ITimelineItemProps) => {
-    const classes = useStyles();
-    const Icon = iconType || IoBriefcaseOutline;
+  const classes = useStyles();
+  const Icon = iconType || IoBriefcaseOutline;
+
+  const getContent = (project: IProject) => {
     return (
+      <CardContent className={classes.projectContent}>
+        <Typography variant="h6" component="h5" className={classes.projectTitle} gutterBottom={true}>{project.title}</Typography>
+        <Typography variant="body2" className={classes.projectDescription}>{project.description}</Typography>
+      </CardContent>
+    )
+  }
+  return (
 
-        <div className={classes.timeline}>
-            {!continuation && <Icon className={classes.boxIcon} />}
-            {!continuation && <Typography variant="h5" component="h3" className={classes.boxTitle}>{item.title}</Typography>}
-            <Typography variant="caption" component="div" className={classes.boxCaption}>
-                {item.startDate} - {item.endDate || 'Present'}
-            </Typography>
-            
-            <Typography variant="h6" component="h4" className={classes.boxSubtitle} gutterBottom={true}>{item.role}</Typography>
-            {item.description && (
-                <Typography variant="body2" className={classes.boxText}>
-                    {item.description}
-                </Typography>
-            )}
-            {item.projects?.map((project) => {
-                return (
-                    <Card key={project.title} className={classes.project}>
-                        <CardContent className={classes.projectContent}>
-                            <Typography variant="h6" component="h5" className={classes.projectTitle} gutterBottom={true}>{project.title}</Typography>
-                            <Typography variant="body2" className={classes.projectDescription}>{project.description}</Typography>
-                        </CardContent>
-                    </Card>
-                );
-            })}
-        </div>
+    <div className={classes.timeline}>
+      {!continuation && <Icon className={classes.boxIcon} />}
+      {!continuation && <Typography variant="h5" component="h3" className={classes.boxTitle}>{item.title}</Typography>}
+      <Typography variant="caption" component="div" className={classes.boxCaption}>
+        {item.startDate} - {item.endDate || 'Present'}
+      </Typography>
 
-    );
+      <Typography variant="h6" component="h4" className={classes.boxSubtitle} gutterBottom={true}>{item.role}</Typography>
+      {item.description && (
+        <Typography variant="body2" className={classes.boxText}>
+          {item.description}
+        </Typography>
+      )}
+      {item.projects?.map((project) => {
+        return (
+          <Card key={project.title} className={classes.project}>
+            {project.url ? (
+              <CardActionArea href={project.url} target="_blank" className={classes.projectAction}>
+                {project.image && (
+                  <Box className={classes.projectImageContent}>
+                    <img src={project.image} className={classes.projectImage} />
+                  </Box>
+                )}
+                {getContent(project)}
+              </CardActionArea>
+            ) : getContent(project)}
+          </Card>
+        );
+      })}
+    </div>
+
+  );
 }
 
 export default TimelineItem;
